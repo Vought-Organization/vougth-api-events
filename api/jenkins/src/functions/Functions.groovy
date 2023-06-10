bdf8f2104d5861685756140f2821cd2c8c53a728
Adalberto Nascimento <nascimentoadalberto678@gmail.com>
2023-06-10 20:36:07 -0300

    feat (Jenkinsfile): ajustes para o pipeline do Jenkins

--- api/jenkins/src/functions/Functions.groovy
+++ api/jenkins/src/functions/Functions.groovy
@@ -1,23 +1,16 @@
 package libs.functions
 
-def String cloneAndCheckout(String REPO, String REPO_NAME, String BRANCH){
-    script {
-        sh "rm -rf *"
+class Functions {
+    String cloneAndCheckout(String REPO, String REPO_NAME, String BRANCH){
+        def PWD
         sh "git clone -b ${BRANCH} ${REPO} && cd ${REPO_NAME} && git checkout ${BRANCH}"
-        PWD = sh (
-                script:  "cd ${REPO_NAME} && pwd",
-                returnStdout: true
-        ).trim()
+        PWD = sh (script:  "cd ${REPO_NAME} && pwd", returnStdout: true).trim()
         return PWD
     }
-}
 
-def String getShortCommitHash(String REPO_PWD) {
-    script {
-        shortCommit = sh (
-                script:  "cd ${REPO_PWD} && git rev-parse --short HEAD",
-                returnStdout: true
-        ).trim()
+    String getShortCommitHash(String REPO_PWD) {
+        def shortCommit
+        shortCommit = sh (script:  "cd ${REPO_PWD} && git rev-parse --short HEAD", returnStdout: true).trim()
         return shortCommit
     }
 }
